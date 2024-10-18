c052905eaf54c7008aaddc4e794512c0b5486767
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-10-18 21:44:14 +0900

    add: ヘッダーにリンクを作成

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>
+          <Link href="/">ホーム</Link>
+          <Link href="/private">プライベート</Link>
+          <Link href="/login">ログイン</Link>
+        </header>
         {children}
       </body>
     </html>
